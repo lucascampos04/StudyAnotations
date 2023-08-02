ff7ca7c5d10e03614c176670feb20975eee8acf2
lucascampos04 <camposdlucasoli@gmail.com>
2023-08-02 17:47:54 -0300

    project basis

---
+++ src/main/java/com/learning/Learning/Study/LearningStudyApplication.java
@@ -0,0 +1,13 @@
+package com.learning.Learning.Study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/Learning/Study/LearningStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.Learning.Study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
